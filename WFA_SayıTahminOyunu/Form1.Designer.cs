dd05dee95c192220573d5ef9455d2f89458823fd
Barış Çelik <bariscelik294534@gmail.com>
2022-04-02 01:49:58 +0300

    Add project files.

---
+++ WFA_SayıTahminOyunu/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace WFA_SayıTahminOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGiris = new System.Windows.Forms.TextBox();
+            this.btnTahmin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtGiris
+            // 
+            this.txtGiris.Location = new System.Drawing.Point(88, 56);
+            this.txtGiris.Name = "txtGiris";
+            this.txtGiris.Size = new System.Drawing.Size(132, 20);
+            this.txtGiris.TabIndex = 0;
+            // 
+            // btnTahmin
+            // 
+            this.btnTahmin.Location = new System.Drawing.Point(88, 146);
+            this.btnTahmin.Name = "btnTahmin";
+            this.btnTahmin.Size = new System.Drawing.Size(132, 42);
+            this.btnTahmin.TabIndex = 1;
+            this.btnTahmin.Text = "Tahmin Et";
+            this.btnTahmin.UseVisualStyleBackColor = true;
+            this.btnTahmin.Click += new System.EventHandler(this.btnTahmin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 307);
+            this.Controls.Add(this.btnTahmin);
+            this.Controls.Add(this.txtGiris);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGiris;
+        private System.Windows.Forms.Button btnTahmin;
+    }
+}
+
---
+++ WFA_SayıTahminOyunu/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_SayıTahminOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+
+         Kullanıcının 5 hakkı olsun. Bilgisayar bir sayı tahmin etsin...Kullanıcı textbox'a sayı yazıp butona basarak bilgisayarın tahmin ettigi sayıyı bulmaya calıssın...Kullanıcı eger bilgisayarın tahmin ettigi sayıdan daha kücük veya büyük bir sayı yazmıssa MessaBox ile ona ipucu verilsin(daha büyük veya kücük sayı tahmin ediniz)...Eger kullanıcı bilgisayarın tahmin ettigi sayıyı bilirse oyunu kazandınız tekrar oynamak ister misiniz diye sorulsun...Kullanıcı eger evet derse programı bastan baslatma kodu Application.Restart()....Hayır derse programı kapatın...Eger kullanıcının hakları bitmişse kaybettiniz sayıyı bilemediniz diyerek program kapansın...Kullanıcının hakları sürekli olarak Form yazısında gözüksün...
+
+         */
+
+        Random rnd = new Random();
+        int hak = 5, bilgisayarTahmini;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bilgisayarTahmini = rnd.Next(1, 11); // 1 dahil, 11 dahil değil.
+            Text = hak.ToString(); // Form'un Text'inde yazacak.
+        }
+
+        private void btnTahmin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int kullaniciTahmini = Convert.ToInt32(txtGiris.Text);
+
+                if (kullaniciTahmini < bilgisayarTahmini)
+                {
+                    hak--;
+                    Text = hak.ToString();
+                    if (hak == 0)
+                    {
+                        DialogResult dr = MessageBox.Show("Kaybettiniz tekrar oynamak ister misiniz", "Kayıp", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                        if (dr == DialogResult.Yes)
+                        {
+                            Application.Restart();
+                        }
+                        else
+                        {
+                            Application.Exit();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bilemediniz...Bilgisayar tahmini daha büyük");
+
+                    }
+
+
+
+
+                }
+                else if (kullaniciTahmini > bilgisayarTahmini)
+                {
+                    hak--;
+                    Text = hak.ToString();
+                    if (hak == 0)
+                    {
+                        DialogResult dr = MessageBox.Show("Kaybettiniz.. Tekrar oynamak ister misiniz", "Kayıp", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                        if (dr == DialogResult.Yes)
+                        {
+                            Application.Restart();
+                        }
+                        else
+                        {
+                            Application.Exit();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bilemediniz...Bilgisayar tahmini daha kücük");
+
+
+                    }
+
+
+
+                }
+                else
+                {
+                    DialogResult dr = MessageBox.Show("Tebrikler...Sayıyı bildiniz...Tekrar oynamak ister misiniz?", "Zafer", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                    if (dr == DialogResult.Yes)
+                    {
+                        Application.Restart();
+                    }
+                    else
+                    {
+                        Application.Exit();
+                    }
+                }
+
+         
+            }
+            catch (Exception ex)
+            {
+
+
+                MessageBox.Show($"Sistemsel hata {ex.Message}");
+            }
+
+
+
+        }
+    
+    }
+}
